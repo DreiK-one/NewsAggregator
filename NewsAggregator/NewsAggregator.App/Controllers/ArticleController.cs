cf0fd7c95122ccf2e4c176e088bff40321296a45
DreiK-one <Dreikone1@mail.ru>
2022-01-26 22:57:39 +0100

    Add ArticleController

---
+++ NewsAggregator/NewsAggregator.App/Controllers/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NewsAggregator.App.Models;
+using NewsAggregator.Core.Interfaces;
+
+namespace NewsAggregator.App.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IMapper mapper, IArticleService articleService)
+        {
+            _mapper = mapper;
+            _articleService = articleService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var articles = (await _articleService.GetAllNewsAsync())
+                .Select(article => _mapper.Map<ArticleViewModel>(article)).ToList();  
+
+            return View(articles);
+        }
+    }
+}
