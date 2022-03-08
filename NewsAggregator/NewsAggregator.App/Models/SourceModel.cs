878c508b59d29970f53fdbdb908aea1e80b5d734
DreiK-one <Dreikone1@mail.ru>
2022-03-08 12:17:35 +0100

    Add simple validation of SourceModel

--- NewsAggregator/NewsAggregator.App/Models/SourceModel.cs
+++ NewsAggregator/NewsAggregator.App/Models/SourceModel.cs
@@ -4,14 +4,14 @@ namespace NewsAggregator.App.Models
 {
     public class SourceModel : BaseModel
     {
-        [Required(AllowEmptyStrings = false)]
-        [MinLength(4)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field isn't be null")]
+        [MinLength(4, ErrorMessage = "Minimum length of this field is 4")]
         public string Name { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field isn't be null")]
         public string BaseUrl { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field isn't be null")]
         public string RssUrl { get; set; }
     }
 }
