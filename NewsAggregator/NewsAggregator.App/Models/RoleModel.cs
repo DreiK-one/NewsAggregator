25d71cc2f20d07ca768a3de2806c4b44bc268e18
DreiK-one <Dreikone1@mail.ru>
2022-03-08 12:33:41 +0100

    Add simple validation of RoleModel and CategoryModel

--- NewsAggregator/NewsAggregator.App/Models/RoleModel.cs
+++ NewsAggregator/NewsAggregator.App/Models/RoleModel.cs
@@ -1,7 +1,11 @@
-﻿namespace NewsAggregator.App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsAggregator.App.Models
 {
     public class RoleModel : BaseModel
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field isn't be null")]
+        [MinLength(4, ErrorMessage = "Minimum length of this field is 4")]
         public string Name { get; set; }
     }
 }
