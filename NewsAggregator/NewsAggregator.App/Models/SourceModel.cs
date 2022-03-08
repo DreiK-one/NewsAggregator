6b9f264cdfcbc4b27609a52b025356fe694c8263
DreiK-one <Dreikone1@mail.ru>
2022-03-08 12:04:52 +0100

    Change views for validation

--- NewsAggregator/NewsAggregator.App/Models/SourceModel.cs
+++ NewsAggregator/NewsAggregator.App/Models/SourceModel.cs
@@ -1,9 +1,17 @@
-﻿namespace NewsAggregator.App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsAggregator.App.Models
 {
     public class SourceModel : BaseModel
     {
+        [Required(AllowEmptyStrings = false)]
+        [MinLength(4)]
         public string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string BaseUrl { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string RssUrl { get; set; }
     }
 }
