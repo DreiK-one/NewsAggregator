cb01a8fcfb88c968ebbdf90635ff6748b0af3fb6
DreiK-one <Dreikone1@mail.ru>
2022-03-09 11:56:04 +0100

    Add Startup class and refactoring

--- NewsAggregator/NewsAggregator.App/Program.cs
+++ NewsAggregator/NewsAggregator.App/Program.cs
@@ -1,124 +1,46 @@
-using Hangfire;
-using Hangfire.SqlServer;
-using Microsoft.EntityFrameworkCore;
-using NewsAggregator.Core.Interfaces;
-using NewsAggregator.Core.Interfaces.Data;
 using NewsAggregator.Data;
-using NewsAggregator.Data.Entities;
-using NewsAggregator.DataAccess;
-using NewsAggregator.Domain.Services;
 using Serilog;
 using Serilog.Events;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-    .WriteTo.Console()
-    .Enrich.FromLogContext()
-    .CreateBootstrapLogger();
 
-try
+namespace NewsAggregator.App
 {
-    Log.Information("Starting web host");
-
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Full setup of serilog. We read log settings from appsettings.json
-    builder.Host.UseSerilog((context, services, configuration) => configuration
-        .ReadFrom.Configuration(context.Configuration)
-        .ReadFrom.Services(services)
-        .Enrich.FromLogContext());
-
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<NewsAggregatorContext>(options => options.UseSqlServer(connectionString));
-
-    // Add services to the container.
-
-    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-    builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
-    builder.Services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
-    builder.Services.AddScoped<IBaseRepository<Comment>, CommentRepository>();
-    builder.Services.AddScoped<IBaseRepository<Role>, RoleRepository>();
-    builder.Services.AddScoped<IBaseRepository<Source>, SourceRepository>();
-    builder.Services.AddScoped<IBaseRepository<User>, UserRepository>();
-    builder.Services.AddScoped<IBaseRepository<UserActivity>, UserActivityRepository>();
-
-    builder.Services.AddScoped<IArticleService, ArticleService>();
-    builder.Services.AddScoped<ICategoryService, CategoryService>();
-    builder.Services.AddScoped<ICommentService, CommentService>();
-    builder.Services.AddScoped<IRoleService, RoleService>();
-    builder.Services.AddScoped<ISourceService, SourceService>();
-    builder.Services.AddScoped<IUserService, UserService>();
-    builder.Services.AddScoped<IRssService, RssService>();
-    builder.Services.AddScoped<IHtmlParserService, HtmlParserService>();
-
-    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-    builder.Services.AddHangfire(configuration => configuration
-        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-        .UseSimpleAssemblyNameTypeSerializer()
-        .UseRecommendedSerializerSettings()
-        .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"),
-            new SqlServerStorageOptions
+    public class Program
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog((context, services, configuration) => configuration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext())
+            .ConfigureWebHostDefaults(webBuilder =>
             {
-                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
-                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
-                QueuePollInterval = TimeSpan.Zero,
-                UseRecommendedIsolationLevel = true,
-                DisableGlobalLocks = true
-            }));
-    builder.Services.AddHangfireServer();
+                webBuilder.UseStartup<Startup>();
+            });
 
-    builder.Services.AddControllersWithViews();
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .WriteTo.Console()
+                .Enrich.FromLogContext()
+                .CreateBootstrapLogger();
 
-    var app = builder.Build();
-
-    // We want to log all HTTP requests
-    app.UseSerilogRequestLogging(configure =>
-    {
-        configure.MessageTemplate = "HTTP {RequestMethod} {RequestPath} ({UserId}) responded {StatusCode} in {Elapsed:0.0000}ms";
-    }); 
-
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().MigrateDatabase().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
     }
-
-    app.UseHttpsRedirection();
-
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.UseAuthorization();
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-
-    app.UseHangfireDashboard();
-
-    using (var scope = app.Services.CreateScope()) //need refactoring
-    {
-        var rssService = scope.ServiceProvider.GetRequiredService<IRssService>();
-        RecurringJob.AddOrUpdate("Aggregate news", 
-            () => rssService.GetNewsFromSources(),
-            "*/10 * * * *");
-    }
-
-    app.MigrateDatabase().Run();
 }
-
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Host terminated unexpectedly");
-    return 1;
-}
-finally
-{
-    Log.CloseAndFlush();
-}
-return 0;
---
+++ NewsAggregator/NewsAggregator.App/Startup.cs
@@ -0,0 +1,110 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using NewsAggregator.Core.Interfaces;
+using NewsAggregator.Core.Interfaces.Data;
+using NewsAggregator.Data;
+using NewsAggregator.Data.Entities;
+using NewsAggregator.DataAccess;
+using NewsAggregator.Domain.Services;
+using Serilog;
+
+namespace NewsAggregator.App
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+            Configuration = configurationBuilder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<NewsAggregatorContext>(options => options.UseSqlServer(connectionString));
+
+            // Add services to the container.
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IArticleRepository, ArticleRepository>();
+            services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
+            services.AddScoped<IBaseRepository<Comment>, CommentRepository>();
+            services.AddScoped<IBaseRepository<Role>, RoleRepository>();
+            services.AddScoped<IBaseRepository<Source>, SourceRepository>();
+            services.AddScoped<IBaseRepository<User>, UserRepository>();
+            services.AddScoped<IBaseRepository<UserActivity>, UserActivityRepository>();
+
+            services.AddScoped<IArticleService, ArticleService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ICommentService, CommentService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<ISourceService, SourceService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IRssService, RssService>();
+            services.AddScoped<IHtmlParserService, HtmlParserService>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"),
+                    new SqlServerStorageOptions
+                    {
+                        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                        QueuePollInterval = TimeSpan.Zero,
+                        UseRecommendedIsolationLevel = true,
+                        DisableGlobalLocks = true
+                    }));
+            services.AddHangfireServer();
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            app.UseSerilogRequestLogging(configure =>
+            {
+                configure.MessageTemplate = "HTTP {RequestMethod} {RequestPath} ({UserId}) responded {StatusCode} in {Elapsed:0.0000}ms";
+            });
+    
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseHangfireDashboard();
+
+            var rssService = serviceProvider.GetRequiredService<IRssService>();
+            RecurringJob.AddOrUpdate("Aggregate news",
+                () => rssService.GetNewsFromSources(),
+                "*/10 * * * *");
+        }
+    }
+}
