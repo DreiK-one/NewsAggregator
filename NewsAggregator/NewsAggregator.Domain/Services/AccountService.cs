2daec4b132de2043d0b680e51dcb51b839cd8d20
DreiK-one <Dreikone1@mail.ru>
2022-04-07 14:53:40 +0200

    Add logging and try-catch block to account service

--- NewsAggregator/NewsAggregator.Domain/Services/AccountService.cs
+++ NewsAggregator/NewsAggregator.Domain/Services/AccountService.cs
@@ -35,139 +35,235 @@ namespace NewsAggregator.Domain.Services
 
         public async Task<bool> CheckUserWithThatEmailIsExistAsync(string email)
         {
-            string normalizedEmail = email.ToUpperInvariant();
+            try
+            {
+                string normalizedEmail = email.ToUpperInvariant();
 
-            return await _unitOfWork.Users.Get()
-                .AnyAsync(user => 
-                    user.NormalizedEmail
-                       .Equals(normalizedEmail));
+                return await _unitOfWork.Users.Get()
+                    .AnyAsync(user =>
+                        user.NormalizedEmail
+                           .Equals(normalizedEmail));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public async Task<Guid?> GetUserIdByEmailAsync(string email)
         {
-            string normalizedEmail = email.ToUpperInvariant();
+            try
+            {
+                string normalizedEmail = email.ToUpperInvariant();
 
-            return (await (await _unitOfWork.Users.FindBy(user =>
-                user.NormalizedEmail != null && user.NormalizedEmail
-                    .Equals(normalizedEmail))).FirstOrDefaultAsync())?.Id;
+                return (await (await _unitOfWork.Users.FindBy(user =>
+                    user.NormalizedEmail != null && user.NormalizedEmail
+                        .Equals(normalizedEmail))).FirstOrDefaultAsync())?.Id;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public async Task<Guid?> GetUserIdByNicknameAsync(string nickname)
         {
-            string normalizedNickname = nickname.ToUpperInvariant();
+            try
+            {
+                string normalizedNickname = nickname.ToUpperInvariant();
 
-            return (await (await _unitOfWork.Users.FindBy(user =>
-                user.NormalizedNickname != null && user.NormalizedNickname
-                    .Equals(normalizedNickname))).FirstOrDefaultAsync())?.Id;
+                return (await (await _unitOfWork.Users.FindBy(user =>
+                    user.NormalizedNickname != null && user.NormalizedNickname
+                        .Equals(normalizedNickname))).FirstOrDefaultAsync())?.Id;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public async Task<string?> GetUserNicknameByIdAsync(Guid id)
         {
-            return (await _unitOfWork.Users.GetById(id)).Nickname;
+            try
+            {
+                return (await _unitOfWork.Users.GetById(id)).Nickname;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public async Task<Guid> CreateUserAsync(string email, string nickname)
         {
-            var id = Guid.NewGuid();
-            await _unitOfWork.Users.Add(new User 
-            { 
-                Id = id, 
-                Email = email,
-                NormalizedEmail = email.ToUpperInvariant(),
-                Nickname = nickname,
-                NormalizedNickname = nickname.ToUpperInvariant(),
-                RegistrationDate = DateTime.Now
-            });
-            await _unitOfWork.Save();
-            return id;
+            try
+            {
+                var id = Guid.NewGuid();
+                await _unitOfWork.Users.Add(new User
+                {
+                    Id = id,
+                    Email = email,
+                    NormalizedEmail = email.ToUpperInvariant(),
+                    Nickname = nickname,
+                    NormalizedNickname = nickname.ToUpperInvariant(),
+                    RegistrationDate = DateTime.Now
+                });
+                await _unitOfWork.Save();
+                return id;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }           
         }
 
         public async Task<int> SetRoleAsync(Guid userId, string roleName)
         {
-            var roleId = await _roleService.GetRoleIdByNameAsync(roleName);
-            if (roleId == Guid.Empty)
+            try
             {
-                roleId = await _roleService.CreateRole(roleName);                
-            }
+                var roleId = await _roleService.GetRoleIdByNameAsync(roleName);
+                if (roleId == Guid.Empty)
+                {
+                    roleId = await _roleService.CreateRole(roleName);
+                }
 
-            await _unitOfWork.UserRoles.Add(new UserRole
+                await _unitOfWork.UserRoles.Add(new UserRole
                 {
                     Id = Guid.NewGuid(),
                     UserId = userId,
                     RoleId = roleId
                 });
-            return await _unitOfWork.Save();
+                return await _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public async Task<IEnumerable<string>> GetRolesAsync(Guid userId)
         {
-            var userRoleIds = (await _unitOfWork.Users
+            try
+            {
+                var userRoleIds = (await _unitOfWork.Users
                 .GetByIdWithIncludes(userId, user => user.UserRoles))
                 .UserRoles.Select(role => role.RoleId);
 
-            var names = new List<string>();
-            foreach (var userRoleId in userRoleIds)
-            {
-                names.Add(await _roleService.GetRoleNameByIdAsync(userRoleId));
-            }
+                var names = new List<string>();
+                foreach (var userRoleId in userRoleIds)
+                {
+                    names.Add(await _roleService.GetRoleNameByIdAsync(userRoleId));
+                }
 
-            return names;
+                return names;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public async Task<int> SetPasswordAsync(Guid userId, string password)
         {
-            await _unitOfWork.Users.PatchAsync(userId, new List<PatchModel>
+            try
             {
-                new PatchModel()
+                await _unitOfWork.Users.PatchAsync(userId, new List<PatchModel>
                 {
-                    PropertyName = "PasswordHash",
-                    PropertyValue = GetPasswordHash(password, _configuration["ApplicationVariables:Salt"])
-                }
-            });
-            return await _unitOfWork.Save();
+                    new PatchModel()
+                    {
+                        PropertyName = "PasswordHash",
+                        PropertyValue = GetPasswordHash(password, _configuration["ApplicationVariables:Salt"])
+                    }
+                });
+                return await _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public async Task<bool> CheckPassword(string email, string password)
         {
-            var userId = await GetUserIdByEmailAsync(email);
-            if (userId.GetValueOrDefault() != Guid.Empty)
+            try
             {
-                var userPasswordHash = (await _unitOfWork.Users.GetById(userId.GetValueOrDefault())).PasswordHash;
-                if (!string.IsNullOrEmpty(userPasswordHash))
+                var userId = await GetUserIdByEmailAsync(email);
+                if (userId.GetValueOrDefault() != Guid.Empty)
                 {
-                    var enteredPasswordHash = GetPasswordHash(password, _configuration["ApplicationVariables:Salt"]);
-
-                    if (userPasswordHash.Equals(enteredPasswordHash))
+                    var userPasswordHash = (await _unitOfWork.Users.GetById(userId.GetValueOrDefault())).PasswordHash;
+                    if (!string.IsNullOrEmpty(userPasswordHash))
                     {
-                        return true;
+                        var enteredPasswordHash = GetPasswordHash(password, _configuration["ApplicationVariables:Salt"]);
+
+                        if (userPasswordHash.Equals(enteredPasswordHash))
+                        {
+                            return true;
+                        }
                     }
                 }
+                return false;
             }
-            return false;
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            } 
         }
 
         private string GetPasswordHash(string password, string salt)
         {
-            var sha1 = new SHA1CryptoServiceProvider();
-            var sha1Data = sha1.ComputeHash(Encoding.UTF8.GetBytes($"{salt}_{password}"));
-            var hashedPassword = Encoding.UTF8.GetString(sha1Data);
-            return hashedPassword;
+            try
+            {
+                var sha1 = new SHA1CryptoServiceProvider();
+                var sha1Data = sha1.ComputeHash(Encoding.UTF8.GetBytes($"{salt}_{password}"));
+                var hashedPassword = Encoding.UTF8.GetString(sha1Data);
+                return hashedPassword;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public bool ValidateIsEmailExists(string email)
         {
-            string normalizedEmail = email.ToUpperInvariant();
+            try
+            {
+                string normalizedEmail = email.ToUpperInvariant();
 
-            return _unitOfWork.Users.Get()
-                .Any(user => user.NormalizedEmail.Equals(normalizedEmail));
+                return _unitOfWork.Users.Get()
+                    .Any(user => user.NormalizedEmail.Equals(normalizedEmail));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         public bool ValidateIsNicknameExists(string nickname)
         {
-            string normalizedNickname = nickname.ToUpperInvariant();
+            try
+            {
+                string normalizedNickname = nickname.ToUpperInvariant();
 
-            return _unitOfWork.Users.Get()
-                .Any(user => user.NormalizedNickname.Equals(normalizedNickname));
+                return _unitOfWork.Users.Get()
+                    .Any(user => user.NormalizedNickname.Equals(normalizedNickname));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{DateTime.Now}: Exception in {ex.Source}, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                throw;
+            }
         }
     }
 }
