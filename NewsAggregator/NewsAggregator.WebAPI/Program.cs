9611ce7d3d666ceed7f6d0349ac8e59063b58081
DreiK-one <Dreikone1@mail.ru>
2022-04-26 13:36:01 +0200

    Update packages

---
+++ NewsAggregator/NewsAggregator.WebAPI/Program.cs
@@ -0,0 +1,33 @@
+using NewsAggregator.Data;
+using Serilog;
+using Serilog.Events;
+
+
+namespace NewsAggregator.WebAPI
+{
+    public class Program
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog((context, services, configuration) => configuration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext())
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .WriteTo.Console()
+                .Enrich.FromLogContext()
+                .CreateBootstrapLogger();
+
+            Log.Information("Starting web host");
+            CreateHostBuilder(args).Build().MigrateDatabase().Run();
+        }
+    }
+}
---
+++ NewsAggregator/NewsAggregator.WebAPI/Startup.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsAggregator.Core.Interfaces;
+using NewsAggregator.Core.Interfaces.Data;
+using NewsAggregator.Data;
+using NewsAggregator.Data.Entities;
+using NewsAggregator.DataAccess;
+using NewsAggregator.Domain.Services;
+using Microsoft.OpenApi.Models;
+
+namespace NewsAggregator.WebAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<NewsAggregatorContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //services.AddScoped<IArticleRepository, ArticleRepository>();
+            //services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
+            //services.AddScoped<IBaseRepository<Comment>, CommentRepository>();
+            //services.AddScoped<IBaseRepository<Role>, RoleRepository>();
+            //services.AddScoped<IBaseRepository<Source>, SourceRepository>();
+            //services.AddScoped<IBaseRepository<User>, UserRepository>();
+            //services.AddScoped<IBaseRepository<UserActivity>, UserActivityRepository>();
+            //services.AddScoped<IBaseRepository<UserRole>, UserRoleRepository>();
+
+            //services.AddScoped<IArticleService, ArticleService>();
+            //services.AddScoped<ICategoryService, CategoryService>();
+            //services.AddScoped<ICommentService, CommentService>();
+            //services.AddScoped<IRoleService, RoleService>();
+            //services.AddScoped<ISourceService, SourceService>();
+            //services.AddScoped<IUserService, UserService>();
+            //services.AddScoped<IRssService, RssService>();
+            //services.AddScoped<IHtmlParserService, HtmlParserService>();
+            //services.AddScoped<IAccountService, AccountService>();
+            //services.AddScoped<IRateService, RateService>();
+
+            //services.AddScoped<IRequestHandler<GetArticlesByPageQuery, IEnumerable<ArticleDto>>,
+            //        GetArticleByPageQueryHandler>();
+            //services.AddScoped<IRequestHandler<RateArticleCommand, bool>,
+            //        RateArticleCommandHandler>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //    .AddJwtBearer(options =>
+            //    {
+            //        options.SaveToken = true;
+            //        options.RequireHttpsMetadata = true;
+            //        options.TokenValidationParameters = new TokenValidationParameters()
+            //        {
+            //            ValidateIssuerSigningKey = true,
+            //            ValidateIssuer = false,
+            //            ValidateAudience = false,
+            //            ClockSkew = TimeSpan.Zero,
+            //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["AppSettings:Secret"]))
+            //        };
+            //    });
+
+            //services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NewsAggregator.WebAPI", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NewsAggregator.WebAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            //app.UseMiddleware<JwtMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
