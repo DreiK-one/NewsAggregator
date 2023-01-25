dfbe2c614bd93aaabad900c43588c32b45269c03
DreiK-one <Dreikone1@mail.ru>
2023-01-25 19:20:54 +0100

    Delete unusable methods and clean the code

--- NewsAggregator/NewsAggregator.Domain.Tests/Services.Tests/Helpers/TestFunctions.cs
+++ NewsAggregator/NewsAggregator.Domain.Tests/Services.Tests/Helpers/TestFunctions.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Moq;
-using NewsAggregator.Core.Interfaces.Data;
+﻿using Moq;
 using NewsAggregator.Data.Entities;
-using NUnit.Framework.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
 using System.Threading;
 
 
@@ -16,17 +12,6 @@ namespace NewsAggregator.Domain.Tests.Services.Tests.Helpers
         public static Mock<IQueryable<T>> GetMockData<T>(IQueryable<T> testData) where T : BaseEntity
         {
             var mockSet = new Mock<IQueryable<T>>();
-            mockSet.As<IAsyncEnumerable<T>>().Setup(x => x.GetAsyncEnumerator(new CancellationToken())).Returns(new TestAsyncEnumerator<T>(testData.GetEnumerator()));
-            mockSet.As<IQueryable<T>>().Setup(x => x.Provider).Returns(new TestAsyncQueryProvider<T>(testData.Provider));
-            mockSet.As<IQueryable<T>>().Setup(x => x.Expression).Returns(testData.Expression);
-            mockSet.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(testData.ElementType);
-            mockSet.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(testData.GetEnumerator());
-            return mockSet;
-        }
-
-        public static Mock<DbSet<T>> GetDbSet<T>(IQueryable<T> testData) where T : BaseEntity
-        {
-            var mockSet = new Mock<DbSet<T>>();
             mockSet.As<IAsyncEnumerable<T>>().Setup(x => x.GetAsyncEnumerator(new CancellationToken())).Returns(new TestAsyncEnumerator<T>(testData.GetEnumerator()));
             mockSet.As<IQueryable<T>>().Setup(x => x.Provider).Returns(new TestAsyncQueryProvider<T>(testData.Provider));
             mockSet.As<IQueryable<T>>().Setup(x => x.Expression).Returns(testData.Expression);
