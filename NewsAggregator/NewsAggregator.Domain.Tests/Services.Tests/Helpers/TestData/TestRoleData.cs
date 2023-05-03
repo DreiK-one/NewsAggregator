93420d4ea1a49c6c8eebff3afb09bf4a1f8692a5
DreiK-one <Dreikone1@mail.ru>
2023-05-03 18:33:14 +0200

    Add helpers for RoleService tests

---
+++ NewsAggregator/NewsAggregator.Domain.Tests/Services.Tests/Helpers/TestData/TestRoleData.cs
@@ -0,0 +1,33 @@
+﻿using NewsAggregator.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace NewsAggregator.Domain.Tests.Services.Tests.Helpers.TestData
+{
+    public static class TestRoleData
+    {
+        public static IQueryable<Role> Roles
+        {
+            get
+            {
+                return new List<Role>
+                {
+                    new Role
+                    {
+                        Id = new Guid("f1dc4182-9459-49ea-a4d2-98f928c6da98")
+                    },
+                    new Role
+                    {
+                        Id = new Guid("08fe00bc-6f69-48bf-9d52-19a74a7be2f6")
+                    },
+                    new Role
+                    {
+                        Id = new Guid("51d741e4-3f36-481f-b895-e1ff4fcf4743")
+                    }
+                }.AsQueryable();
+            }
+        }
+    }
+}
---
+++ NewsAggregator/NewsAggregator.Domain.Tests/Services.Tests/RoleServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NewsAggregator.Core.Interfaces.Data;
+using NewsAggregator.Domain.Services;
+using NewsAggregator.Domain.Tests.Services.Tests.Helpers.TestData;
+using NewsAggregator.Domain.Tests.Services.Tests.Helpers;
+using NUnit.Framework;
+using NewsAggregator.App.Mappers;
+using NewsAggregator.Core.Interfaces;
+using System.Threading.Tasks;
+using System.Linq;
+using NewsAggregator.Data.Entities;
+using System.Collections.Generic;
+using System;
+using NewsAggregator.Core.DTOs;
+using NewsAggregator.Data;
+
+
+namespace NewsAggregator.Domain.Tests.Services.Tests
+{
+    [TestFixture]
+    public class RoleServiceTests
+    {
+        private IMapper _mapper;
+        private Mock<ILogger<RoleService>> _logger;
+        private Mock<IUnitOfWork> _unitOfWork;
+        private RoleService _roleService;
+
+        public RoleServiceTests()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new RoleProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _logger = new Mock<ILogger<RoleService>>();
+
+            _unitOfWork.Setup(uOw => uOw.Roles.Get())
+                .ReturnsAsync(TestFunctions.GetMockData(TestRoleData.Roles).Object);
+
+            _roleService = new RoleService(_mapper,
+                _logger.Object,
+                _unitOfWork.Object);
+        }
+    }
+}
